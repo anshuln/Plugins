088269a6ea4475c94c9fe3c1a81327d5109e16ff
anshuln <anshulnasery@gmail.com>
2019-11-18 01:47:42 +0530

    Adds config file

---
+++ SublimeText/TODOs/README.md
@@ -0,0 +1,7 @@
+# TODO Helper
+A plugin to track specific comments in your project.
+
+## How to use
+* Copy the entire directory into your `sublime-text-3/Packages` directory
+* Edit the `TODO_config.json` file according to your language and project preferences.
+* Run `TODO` in the command palette to run the package.
--- SublimeText/TODOs/todoHelper.py
+++ SublimeText/TODOs/todoHelper.py
@@ -1,50 +1,59 @@
 import sublime
 import sublime_plugin
 import re
+import json
 
-def process(content,pattern='todo',fn_identifier='def ',comment_identifier='#'):  #TODO read identifier from config
-	function = ''	#TODO make this a stack
+config_path = 'TODO.config'
+def process(content,pattern='todo',done_pattern='DONE',fn_identifier='def ',comment_identifier='#'):  #TODO read identifier from config
+	function = ''   #TODO make this a stack
 	#TODO add similar for class as well
 	#TODO look at indentation levels to figure out when a function ends
 	todos = []
+	view = content
+	content = content.split('\n')
 	for line_id in range(len(content)):
-		text = content[line_id].strip()	#removes leading and trailing whitespaces
+		text = content[line_id].strip() #removes leading and trailing whitespaces
 		if text.find(fn_identifier) != -1:
 			function = text[text.find(fn_identifier)+4:text.find('(')]
 		l = text.lower().find(pattern)
+
 		if l == -1:
 			continue     #TODO figure out way to make this more efficient
 		else:   #TODO figure out how to handle empty context, may add fuction name etc like a traceback.
 			context = text[:l].strip(comment_identifier).strip() + ((' (in function {})'.format(function))*(len(function)>0))
-			if len(context)==0:		#TODO maybe show the next line....
+			if len(context)==0:     #TODO maybe show the next line....
 				context = ((" In function {}".format(function))*(len(function)>0))+(("General TODO")*(len(function)==0))
 
 			comment = text[l+4:]
-			todos.append('Line {}:\t{} \n {} \n'.format(str(line_id+1),context,comment))
-	return todos
+			if comment.find(done_pattern) != -1:  #TODO somehow change the original file to delete the comment
+				view.replace(text,context)
+				continue
+			todos.append('Line {}:{} \n {} \n\n'.format(str(line_id+1),context,comment))
+	return todos,view
 
 
 class TodoCommand(sublime_plugin.TextCommand): 
 	def run(self, edit):   
-		s = self.view.substr(sublime.Region(0,self.view.size())).split('\n')  #assign s variable the selected region
-		TODOs = process(s)
+		config = json.load(open(config_path,'r'))
+		s = self.view.substr(sublime.Region(0,self.view.size()))  
+		TODOs,new_text = process(s,pattern=config['pattern'],done_pattern=config['done_pattern'],fn_identifier=config['fn_identifier'],comment_identifier=config['comment_identifier'])
+		print(self.view.file_name())
 		currentFolder = "/".join(self.view.file_name().split("/")[:-1])
 		fileName = self.view.file_name().split("/")[-1]
 		new_contents = "In file {} \n---------- \n{} \n==========".format(fileName,"".join(TODOs))
 		try:
-			f = open("{}/TODO.txt".format(currentFolder),"r")
+			f = open("{}/{}".format(currentFolder,config["write_file"]),"r")
 			contents = f.read()
 			regex = 'In file {} \n---------- \n[\s\S]*? \n=========='.format(fileName)
 			if re.search(regex,contents) is None:
 				write_to_file = contents + "\n" + new_contents
 			else:
-				write_to_file = re.sub(regex,new_contents,contents) #TODO eliminate this call
+				write_to_file = re.sub(regex,new_contents,contents) #TODO eliminate this call -- DONE
 			f.close()
-		except:
+		except FileNotFoundError:
 			write_to_file = new_contents
 
-		with open("{}/TODO.txt".format(currentFolder),"w") as f:
+		with open("{}/{}".format(currentFolder,config["write_file"]),"w") as f:
 			f.write(write_to_file)
-
-
-#TODO have a config file to look at comment delimiters, can do it by language	
+		self.view.replace(edit,sublime.Region(0,self.view.size()),new_text)
+#TODO have a config file to look at comment delimiters, can do it by language   
